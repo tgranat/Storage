483029656800767a52bcab553b63740d2994a609
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-10-07 15:02:47 +0200

    Adding view model

---
+++ Storage/Models/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public int InventoryValue { get; set; }
+    }
+}
