5dba06a88231f6926aa784f4ce577a4a2fe870d1
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-10-07 13:22:46 +0200

    Create Product

--- Storage/Startup.cs
+++ Storage/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Storage.Data;
 
 namespace Storage
 {
@@ -24,6 +26,9 @@ namespace Storage
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<StorageContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("StorageContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
